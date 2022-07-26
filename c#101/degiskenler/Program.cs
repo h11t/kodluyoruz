b08161f877da3173942d32ce89cb4e0326be14cf
Hilal Tekdemir <hilal.tek.demir00@gmail.com>
2022-07-26 20:00:58 +0300

    ilk 3 ders ve gitignore eklendi

---
+++ c#101/degiskenler/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace degiskenler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            
+            Console.WriteLine(5);
+        }
+    }
+}
