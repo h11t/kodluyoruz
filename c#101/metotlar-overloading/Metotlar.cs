eb2e308340d8af0a7aa9adce837119fd6d8306bc
Hilal Tekdemir <hilal.tek.demir00@gmail.com>
2022-08-09 12:39:24 +0300

    metot overloading dersi eklendi

---
+++ c#101/metotlar-overloading/Metotlar.cs
@@ -0,0 +1,26 @@
+
+class Metotlar
+{
+
+    public void Topla(int a, int b,out int toplam)
+    {
+        toplam=a+b;
+    }
+
+    public void EkranaYazdir(string veri)
+    {
+        Console.WriteLine(veri);
+    }
+
+    public void EkranaYazdir(int veri)
+    {
+        Console.WriteLine(veri);
+    }
+
+    public void EkranaYazdir(string veri1, string veri2)
+    {
+        Console.WriteLine(veri1+veri2);
+    }
+
+}
+
---
+++ c#101/metotlar-overloading/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//out parametreler
+string sayi="999";
+
+bool sonuc=int.TryParse(sayi,out int outsayi);
+if (sonuc)
+{
+    Console.WriteLine("başarılı");
+    Console.WriteLine(outsayi);
+}
+else
+{
+    Console.WriteLine("başarısız");
+
+}
+
+Metotlar instance=new Metotlar();
+instance.Topla(2,3,out int toplamSonuc);
+Console.WriteLine(toplamSonuc);
+
+//metot aşırı yükleme - overloading
+int ifade=999;
+instance.EkranaYazdir(Convert.ToString(ifade));
+instance.EkranaYazdir(ifade);
+instance.EkranaYazdir("Hilal","Tekdem");
+
+//metot imazası= metotadi+parametre sayisi+ parametre tipi
+//erişim belirteci imzaya dahil değildir. aynı imzalı public ve private metotları için hata verir
+
+
