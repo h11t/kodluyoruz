7eb1b5fe2b733a1259701f37a99440191205fbb6
Hilal Tekdemir <hilal.tek.demir00@gmail.com>
2022-08-25 09:16:45 +0300

    ödev2 koleksiyonlar3 kısmı bitirilmiştir

---
+++ c#101/odev2/Koleksiyonlar-Soru-3/Program.cs
@@ -0,0 +1,18 @@
+﻿/*Soru - 3: Klavyeden girilen cümle içerisindeki sesli harfleri bir dizi içerisinde saklayan ve dizinin elemanlarını sıralayan programı yazınız.
+*/
+using System.Collections;
+ArrayList sesliharfler=new ArrayList();
+char[] harfler=(Console.ReadLine()).ToCharArray();
+foreach (var h in harfler)
+{
+    if(h=='a' ||h=='e' ||h=='ı' ||h=='i' ||h=='o' ||h=='ö' ||h=='u' ||h=='ü')
+    {
+        sesliharfler.Add(h);
+    }
+}
+sesliharfler.Sort();
+foreach (var item in sesliharfler)
+{
+    Console.WriteLine(item);
+}
+
